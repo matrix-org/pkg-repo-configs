0c9eede1b311be2d53cd820dfbfde52778160140
Erik Johnston <erikj@jki.re>
2022-08-17 11:00:25 +0100

    Apply suggestions from code review
    Co-authored-by: Sean Quah <8349537+squahtx@users.noreply.github.com>

--- download-latest-release.sh
+++ download-latest-release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Downloads the Debian tarball from the latest Synapse release on GitHub
+# and extracts it into debian/incoming/.
 set -euo pipefail
 
 RELEASE_JSON=$( curl -s "https://api.github.com/repos/matrix-org/synapse/releases" | jq '.[0]' )
@@ -7,7 +9,7 @@ RELEASE_JSON=$( curl -s "https://api.github.com/repos/matrix-org/synapse/release
 VERSION=$( echo $RELEASE_JSON | jq -r '.name' )
 DOWNLOAD_URL=$( echo $RELEASE_JSON | jq -r '.assets | .[] | select(.name == "debs.tar.xz") | .browser_download_url' )
 
-echo "Downloading $VERSION"
+echo "Downloading $VERSION to debian/incoming..."
 
 curl -L --progress-bar "$DOWNLOAD_URL" | tar -C debian/incoming --strip-components=1 -xJ
 
