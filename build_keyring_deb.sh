3003d9094b1615722587e7f4d2554c7c9a67fea2
Erik Johnston <erik@matrix.org>
2023-02-17 16:23:16 +0000

    Add a keyring package

---
+++ build_keyring_deb.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Builds a keyring deb and imports it into the local reprepro DB. 
+#
+# NOTE: If you update the key you will need to bump the version in
+# `matrix-org-archive-keyring/DEBIAN/control`.
+
+set -euo pipefail
+
+mkdir -p matrix-org-archive-keyring/usr/share/keyrings/
+
+echo "Downloading keyring.gpg..."
+wget "https://packages.matrix.org/debian/matrix-org-archive-keyring.gpg" -O matrix-org-archive-keyring/usr/share/keyrings/matrix-org-archive-keyring.gpg
+
+echo "Downloaded key:"
+gpg --import --import-options show-only matrix-org-archive-keyring/usr/share/keyrings/matrix-org-archive-keyring.gpg
+
+read -p "Press any key to continue..."
+
+echo "Building deb..."
+dpkg-deb --build matrix-org-archive-keyring matrix-org-archive-keyring.deb
+
+for dist in $(ls -1 "packages.matrix.org/debian/dists")
+do
+	echo "Importing deb into $dist..."
+	reprepro -b debian/ -C "main"  includedeb  $dist matrix-org-archive-keyring.deb
+	reprepro -b debian/ -C "prerelease"  includedeb  $dist matrix-org-archive-keyring.deb
+done
+
+echo "Done!"
